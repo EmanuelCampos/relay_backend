b775fc52cd66d6875ffc8cabebd1bd7d50d24927
EMANUEL <emanuel@EMANUELs-Mac-mini.local>
2021-07-26 21:44:12 -0300

    poc of relay

---
+++ src/graphql/post/mutations/CreatePost.ts
@@ -0,0 +1,83 @@
+import { GraphQLNonNull, GraphQLString } from "graphql";
+import { mutationWithClientMutationId, toGlobalId } from "graphql-relay";
+
+import fs from 'fs';
+import { uuid } from 'uuidv4';
+import { resolve } from 'path';
+import { readDatabase } from "../../../utils";
+import { PostEdge } from "../PostType";
+
+export default mutationWithClientMutationId({
+  name: 'CreatePost',
+  inputFields: {
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    body: {
+      type: new GraphQLNonNull(GraphQLString),
+    }
+  },
+  mutateAndGetPayload: async({ title, body }) => {
+    const source = resolve(__dirname, '..', '..', '..', '..');
+
+    const data = await readDatabase();
+
+    const post = {
+      id: uuid(),
+      title,
+      body
+    }
+
+    let posts = [];
+
+    if(data.posts) {
+      posts = [
+        ...data.posts,
+        {
+          ...post
+        }
+      ]
+    } else {
+      posts = [
+        {
+          ...post
+        }
+      ]
+    }
+
+    await fs.writeFileSync(`${source}/data.json`, JSON.stringify({
+      ...data,
+      posts
+    }));
+
+    return {
+      post,
+      error: null,
+      success: "Post created successfully"
+    }
+
+  },
+  outputFields: {
+    postEdge: {
+      type: PostEdge,
+      resolve: async ({ post }) => {
+        if(!post) {
+          return null;
+        }
+
+        return {
+          cursor: toGlobalId('Post', post.id),
+          node: post
+        }
+      }
+    },
+    error: {
+      type: GraphQLString,
+      resolve: ({ error }: { error: string }) => error,
+    },
+    success: {
+      type: GraphQLString,
+      resolve: ({ success }: { success: string }) => success,
+    },
+  }
+})
